c20569a4d95a2a83e2b17e2b366d44f33acb6399
cngzylmz89 <muallimiturki@gmail.com>
2026-04-02 19:43:04 +0300

    testtakip

---
+++ TESTTAKIP/TESTTAKIP/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace TESTTAKIP
+{
+    partial class frmtest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splana = new System.Windows.Forms.SplitContainer();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.splaltana = new System.Windows.Forms.SplitContainer();
+            this.splaltanasol = new System.Windows.Forms.SplitContainer();
+            this.pcksoru = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblsure = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.splaltanasag = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splana)).BeginInit();
+            this.splana.Panel1.SuspendLayout();
+            this.splana.Panel2.SuspendLayout();
+            this.splana.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splaltana)).BeginInit();
+            this.splaltana.Panel1.SuspendLayout();
+            this.splaltana.Panel2.SuspendLayout();
+            this.splaltana.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splaltanasol)).BeginInit();
+            this.splaltanasol.Panel1.SuspendLayout();
+            this.splaltanasol.Panel2.SuspendLayout();
+            this.splaltanasol.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcksoru)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splaltanasag)).BeginInit();
+            this.splaltanasag.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splana
+            // 
+            this.splana.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splana.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splana.Location = new System.Drawing.Point(0, 0);
+            this.splana.Margin = new System.Windows.Forms.Padding(6);
+            this.splana.Name = "splana";
+            this.splana.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splana.Panel1
+            // 
+            this.splana.Panel1.Controls.Add(this.tableLayoutPanel1);
+            // 
+            // splana.Panel2
+            // 
+            this.splana.Panel2.Controls.Add(this.splaltana);
+            this.splana.Size = new System.Drawing.Size(2407, 1276);
+            this.splana.SplitterDistance = 71;
+            this.splana.SplitterWidth = 8;
+            this.splana.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.01766F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 88.98234F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(6);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 71F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(2407, 71);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::TESTTAKIP.Properties.Resources.icon1;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 6);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(253, 59);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(271, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(2130, 71);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TEST TAKİP";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // splaltana
+            // 
+            this.splaltana.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splaltana.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splaltana.Location = new System.Drawing.Point(0, 0);
+            this.splaltana.Margin = new System.Windows.Forms.Padding(6);
+            this.splaltana.Name = "splaltana";
+            // 
+            // splaltana.Panel1
+            // 
+            this.splaltana.Panel1.Controls.Add(this.splaltanasol);
+            // 
+            // splaltana.Panel2
+            // 
+            this.splaltana.Panel2.Controls.Add(this.splaltanasag);
+            this.splaltana.Size = new System.Drawing.Size(2407, 1197);
+            this.splaltana.SplitterDistance = 2099;
+            this.splaltana.SplitterWidth = 8;
+            this.splaltana.TabIndex = 0;
+            // 
+            // splaltanasol
+            // 
+            this.splaltanasol.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splaltanasol.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splaltanasol.Location = new System.Drawing.Point(0, 0);
+            this.splaltanasol.Margin = new System.Windows.Forms.Padding(6);
+            this.splaltanasol.Name = "splaltanasol";
+            this.splaltanasol.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splaltanasol.Panel1
+            // 
+            this.splaltanasol.Panel1.Controls.Add(this.pcksoru);
+            // 
+            // splaltanasol.Panel2
+            // 
+            this.splaltanasol.Panel2.Controls.Add(this.tableLayoutPanel2);
+            this.splaltanasol.Size = new System.Drawing.Size(2099, 1197);
+            this.splaltanasol.SplitterDistance = 1088;
+            this.splaltanasol.SplitterWidth = 8;
+            this.splaltanasol.TabIndex = 0;
+            // 
+            // pcksoru
+            // 
+            this.pcksoru.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pcksoru.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pcksoru.Location = new System.Drawing.Point(0, 0);
+            this.pcksoru.Margin = new System.Windows.Forms.Padding(6);
+            this.pcksoru.Name = "pcksoru";
+            this.pcksoru.Size = new System.Drawing.Size(2099, 1088);
+            this.pcksoru.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcksoru.TabIndex = 0;
+            this.pcksoru.TabStop = false;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 52.6047F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 47.3953F));
+            this.tableLayoutPanel2.Controls.Add(this.lblsure, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.progressBar1, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(6);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 97F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(2099, 101);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // lblsure
+            // 
+            this.lblsure.AutoSize = true;
+            this.lblsure.BackColor = System.Drawing.Color.Black;
+            this.lblsure.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblsure.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblsure.ForeColor = System.Drawing.Color.Yellow;
+            this.lblsure.Location = new System.Drawing.Point(1110, 0);
+            this.lblsure.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblsure.Name = "lblsure";
+            this.lblsure.Size = new System.Drawing.Size(983, 101);
+            this.lblsure.TabIndex = 1;
+            this.lblsure.Text = "label2";
+            this.lblsure.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar1.Location = new System.Drawing.Point(6, 6);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(6);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1092, 89);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // splaltanasag
+            // 
+            this.splaltanasag.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splaltanasag.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splaltanasag.Location = new System.Drawing.Point(0, 0);
+            this.splaltanasag.Name = "splaltanasag";
+            this.splaltanasag.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.splaltanasag.Size = new System.Drawing.Size(300, 1197);
+            this.splaltanasag.SplitterDistance = 1092;
+            this.splaltanasag.TabIndex = 0;
+            // 
+            // frmtest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2407, 1276);
+            this.Controls.Add(this.splana);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "frmtest";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmtest_Load);
+            this.splana.Panel1.ResumeLayout(false);
+            this.splana.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splana)).EndInit();
+            this.splana.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.splaltana.Panel1.ResumeLayout(false);
+            this.splaltana.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splaltana)).EndInit();
+            this.splaltana.ResumeLayout(false);
+            this.splaltanasol.Panel1.ResumeLayout(false);
+            this.splaltanasol.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splaltanasol)).EndInit();
+            this.splaltanasol.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pcksoru)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splaltanasag)).EndInit();
+            this.splaltanasag.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splana;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SplitContainer splaltana;
+        private System.Windows.Forms.SplitContainer splaltanasol;
+        private System.Windows.Forms.PictureBox pcksoru;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label lblsure;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.SplitContainer splaltanasag;
+    }
+}
+
---
+++ TESTTAKIP/TESTTAKIP/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TESTTAKIP
+{
+    public partial class frmtest : Form
+    {
+        public frmtest()
+        {
+            InitializeComponent();
+        }
+        int saniye = 150; // Toplam süre (saniye cinsinden)
+        int totalTimeMs;
+        int elapsedTime = 0;
+
+        private void frmtest_Load(object sender, EventArgs e)
+        {
+            totalTimeMs = saniye * 1000; // Toplam süreyi milisaniyeye çeviriyoruz
+            // ProgressBar ayarları
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = totalTimeMs; // toplam süreyi max yapıyoruz
+            progressBar1.Value = 0;
+            progressBar1.Style = ProgressBarStyle.Continuous;
+
+            timer1.Interval = 50; // akıcı animasyon
+            timer1.Start();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            elapsedTime += timer1.Interval;
+
+            if (elapsedTime <= totalTimeMs)
+            {
+                progressBar1.Value = Math.Min(elapsedTime, totalTimeMs);
+
+                // Renk geçişi
+                double oran = (double)elapsedTime / totalTimeMs;
+
+                if (oran <= 0.5) // ilk yarı → yeşil
+                {
+                    progressBar1.ForeColor = Color.Green;
+                }
+                else if (oran <= 0.8) // ikinci kısım → sarı
+                {
+                    progressBar1.ForeColor = Color.Yellow;
+                }
+                else // son kısım → kırmızı
+                {
+                    progressBar1.ForeColor = Color.Red;
+                }
+
+                // Kalan süre label
+                int kalanMs = totalTimeMs - elapsedTime;
+                lblsure.Text = TimeSpan.FromMilliseconds(kalanMs).ToString(@"mm\:ss");
+            }
+            else
+            {
+                timer1.Stop();
+                progressBar1.Value = totalTimeMs;
+                lblsure.Text = "00:00";
+                MessageBox.Show("Süre doldu!");
+            }
+        }
+    }
+    
+}
